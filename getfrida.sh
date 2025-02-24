6261df7136df64749f22930d4ea89d19e6904737
ax <ax.tryin@gmail.com>
2025-02-25 00:33:54 +0100

    frida 16.5.6 stuff

---
+++ getfrida.sh
@@ -0,0 +1,7 @@
+FRIDA_VERSION="16.5.6"
+wget https://github.com/frida/frida/releases/download/16.5.6/frida-core-devkit-16.5.6-linux-x86_64.tar.xz
+unxz frida-core-devkit-16.5.6-linux-x86_64.tar.xz 
+tar xvf frida-core-devkit-16.5.6-linux-x86_64.tar
+
+wget https://github.com/frida/frida/releases/download/16.5.6/frida-server-16.5.6-linux-x86_64.xz
+unxz frida-server-16.5.6-linux-x86_64.xz
